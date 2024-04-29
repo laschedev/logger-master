cf508b2485d9d9e0357cd3a59c23dec7c8b96547
lasche <166583824+laschedev@users.noreply.github.com>
2024-04-29 09:32:28 +0300

    Update logger-master.js

--- logger-master.js
+++ logger-master.js
@@ -6,15 +6,9 @@ const levels = {
     default: { color: "\x1b[36m", name: "default" }
 };
 
-const sendLog = (message, level = "default") => {
-    const currentDate = new Date();
-    const hours = String(currentDate.getHours()).padStart(2, "0");
-    const minutes = String(currentDate.getMinutes()).padStart(2, "0");
-    const seconds = String(currentDate.getSeconds()).padStart(2, "0");
-    const time = `${hours}:${minutes}:${seconds}`;
-
-    const selectedLevel = levels[level] || levels.default;
-    const { color, name: levelName } = selectedLevel;
-
-    console.log(`[\x1b[36m${time}\x1b[0m] ${color}[${levelName}]\x1b[0m \x1b[35m>\x1b[0m ${message}`);
+const sendLog = (message, level) => {
+    const { color, name: levelName } = levels[level] || levels.default;
+    console.log(`[\x1b[36m${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}\x1b[0m] ${color}[${levelName}]\x1b[0m \x1b[35m>\x1b[0m ${message}`);
 };
+
+module.exports = sendLog;
