018525214d2416e21ee1b13299328be5051d0d8d
lasche <166583824+laschedev@users.noreply.github.com>
2024-05-17 20:15:00 +0300

    Update logger-master.js

--- logger-master.js
+++ logger-master.js
@@ -1,3 +1,27 @@
+/**
+ * A logging module that supports different log levels and colors.
+ * @module the-logger-master
+ */
+
+/**
+ * Log levels and their corresponding colors.
+ * @constant {Object} levels
+ * @property {Object} error - Error log level.
+ * @property {string} error.color - ANSI escape code for red color.
+ * @property {string} error.name - Name of the log level.
+ * @property {Object} warn - Warning log level.
+ * @property {string} warn.color - ANSI escape code for yellow color.
+ * @property {string} warn.name - Name of the log level.
+ * @property {Object} info - Info log level.
+ * @property {string} info.color - ANSI escape code for green color.
+ * @property {string} info.name - Name of the log level.
+ * @property {Object} debug - Debug log level.
+ * @property {string} debug.color - ANSI escape code for blue color.
+ * @property {string} debug.name - Name of the log level.
+ * @property {Object} default - Default log level.
+ * @property {string} default.color - ANSI escape code for cyan color.
+ * @property {string} default.name - Name of the log level.
+ */
 const levels = {
     error: { color: "\x1b[31m", name: "error" },
     warn: { color: "\x1b[33m", name: "warn" },
@@ -6,9 +30,17 @@ const levels = {
     default: { color: "\x1b[36m", name: "default" }
 };
 
+/**
+ * Logs a message with the specified log level.
+ * @function sendLog
+ * @param {string} message - The message to log.
+ * @param {string} [level="default"] - The log level (error, warn, info, debug, default).
+ * @returns {void}
+ */
 const sendLog = (message, level) => {
-    const { color, name: levelName } = levels[level] || levels.default;
-    console.log(`[\x1b[36m${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}\x1b[0m] ${color}[${levelName}]\x1b[0m \x1b[35m>\x1b[0m ${message}`);
+    const currentTime = new Date().toLocaleTimeString(); // get the current time
+    const { color, name: levelName } = levels[level] || levels.default; // get the level
+    console.log(`[\x1b[36m${currentTime}\x1b[0m] ${color}[${levelName}]\x1b[0m \x1b[35m>\x1b[0m ${message}`); // send the log message
 };
 
 module.exports = sendLog;
